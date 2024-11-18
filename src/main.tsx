84d6ae05f2c7341c73aa92c7696d95fc4a9bb781
yslati <kaliyassin01@gmail.com>
2024-11-18 10:56:17 +0100

    setup project structer

--- src/main.tsx
+++ src/main.tsx
@@ -3,7 +3,7 @@ import { createRoot } from 'react-dom/client'
 import './index.css'
 import App from './App.tsx'
 import { Provider } from 'react-redux'
-import store from './app/store.ts'
+import store from './store/store.ts'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
---
+++ src/pages/Dashboard.tsx
@@ -0,0 +1,9 @@
+const Dashboard = () => {
+    return (
+        <div className="">
+            dashboard
+        </div>
+    );
+}
+ 
+export default Dashboard;
